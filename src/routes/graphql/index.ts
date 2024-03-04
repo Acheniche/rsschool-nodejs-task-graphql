7d28d6362bcdef24affc9280ca8de56a1d9766f4
Acheniche <ache.niche00@mail.ru>
2024-03-04 17:07:30 +0300

    feat: add graphql-depth-limit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -2,6 +2,7 @@ import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { graphql, GraphQLSchema, parse, validate } from 'graphql';
 import { Query } from './query.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -18,6 +19,11 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         query: Query,
       });
 
+      const errors = validate(schema, parse(req.body.query), [depthLimit(5)]);
+      if (errors.length > 0) {
+      return { data: null, errors: errors };
+      }
+      
       return await graphql({
         schema,
         source: String(req.body.query),
